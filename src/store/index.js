70958fa4a3da1fabaa5b4c96a9c90d0fd12c5923
Patrick Duque <patpatduque26@gmail.com>
2020-08-11 22:59:30 +0800

    removed compose

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
-const composeEnhancers =
-  process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : null;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
